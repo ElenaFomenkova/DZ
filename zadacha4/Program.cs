0d4455731b4e45e2cb8b00fff70d8e79e59513b2
Elena <Fomenkova.len@ya.ru>
2022-11-03 20:03:54 +0300

    Zadacha4

---
+++ zadacha4/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = 2;
+int b = 3;
+int c = 7;
+
+int max = a;
+
+if (b > a)
+{
+    max = b;
+    if (c > b)
+    {
+        max = c;
+    }
+}
+else
+{
+    if (c > a)
+    {
+        max = c;
+    }
+}
+
+Console.WriteLine("max = " + max);
