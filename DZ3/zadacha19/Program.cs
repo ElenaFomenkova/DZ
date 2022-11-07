6f801c32f46365737ee66d476da2a755ad928200
Elena <Fomenkova.len@ya.ru>
2022-11-07 17:01:10 +0300

    zadacha19

---
+++ DZ3/zadacha19/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 12821;
+int k1 = a % 10;
+int k2 = (a % 100) / 10;
+int k3 = (a % 10000) / 1000;
+int k4 = a / 10000;
+if (k1 == k4 && k2 == k3)
+{
+    Console.WriteLine("yes");
+}
+else {
+    Console.WriteLine("no");
+}
+    
