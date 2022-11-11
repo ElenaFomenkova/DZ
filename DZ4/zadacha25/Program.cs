a2d48e5cf474dfaf54bf2d0c5f0e3a77f2e17514
Elena <Fomenkova.len@ya.ru>
2022-11-11 12:38:25 +0300

    zadacha25

---
+++ DZ4/zadacha25/Program.cs
@@ -0,0 +1,17 @@
+﻿double Pow(int a, int b)
+{
+    if (a == 0 && b == 0) return double.NaN;
+    bool flag;
+    flag = b > 0;
+    if (b < 0) b = -b;
+    int res = 1;
+    for (int i = 0; i < b; i++)
+    {
+        res = res * a;
+    }
+    if (flag) return res;
+    else return 1.0 / res;
+}
+
+System.Console.WriteLine(Pow(2, 4));
+  
