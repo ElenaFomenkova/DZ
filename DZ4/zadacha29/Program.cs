516179e7c0dee4a396b92175a0e9fa325dfb8337
Elena <Fomenkova.len@ya.ru>
2022-11-11 17:30:30 +0300

    zadacha29

---
+++ DZ4/zadacha29/Program.cs
@@ -0,0 +1,15 @@
+﻿int n;
+Console.Write("Введите количество чисел ");
+n = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[n];
+Random rand = new Random();
+for (int i = 0; i < n; i++)
+{
+    Array[i] = rand.Next(-10, 10);
+}
+
+for (int i = 0; i < n; i++)
+{
+
+    Console.Write(Array[i] + " ");
+}
