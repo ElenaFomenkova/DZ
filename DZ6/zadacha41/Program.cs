f945d0a5ed9321ed3a9cbdc277eab3c7c6d16628
Elena <Fomenkova.len@ya.ru>
2022-11-15 23:58:46 +0300

    zadacha41

---
+++ DZ6/zadacha41/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите элементы через пробел");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int cnt = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        cnt = cnt + 1;
+    }
+}
+
+Console.WriteLine($"Количество элементов > 0: {cnt}");
