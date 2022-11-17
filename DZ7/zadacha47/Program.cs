c984f58032acf0b069d4b141314b8dbd76834b76
Elena <Fomenkova.len@ya.ru>
2022-11-18 01:11:56 +0300

    readme

---
+++ DZ7/zadacha47/Program.cs
@@ -0,0 +1,12 @@
+﻿//Создание двумерного массива.
+int[,] createArray(int n, int m)
+{
+    int[,] array = new int[n, m]; return array;
+    return array;
+}
+
+//Заполнение массива.
+int[,] fillArray(int[,] array){
+    Random rand = new Random();
+}
+return Array;
