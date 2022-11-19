0db6249d9c1438820c904302ea871e3192a82360
Elena <Fomenkova.len@ya.ru>
2022-11-20 01:06:15 +0300

    zadacha52

--- DZ7/zadacha52/Program.cs
+++ DZ7/zadacha52/Program.cs
@@ -1,10 +1,4 @@
-﻿Console.Write("Введите количество строк массива");
-int n = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Введите количество столбцов массива");
-int m = Convert.ToInt32(Console.ReadLine());
-
-//Создание двумерного массива.
+﻿//Создание двумерного массива.
 int[,] createArray(int n, int m)
 {
     int[,] array = new int[n, m];
@@ -29,7 +23,35 @@ void printArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 1; j < array.GetLength(1); j++) ;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
     }
-    Console.Write(array[i, j] + "\t");
 }
+// Подсчет среднего арифметического
+void GetAwerageArray(int[,] inputArray)
+{
+
+    for (int i = 0; i < inputArray.GetLength(1); i++)
+    {
+        float sum = 0;
+        for (int j = 0; j < inputArray.GetLength(0); j++)
+        {
+            sum = sum + inputArray[j, i];
+        }
+        Console.WriteLine(sum/inputArray.GetLength(0));
+    }
+}
+
+Console.Write("Введите количество строк массива");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] Array = createArray(n, m);
+Array = fillArray(Array, -10, 10);
+printArray(Array);
+
+GetAwerageArray(Array);
