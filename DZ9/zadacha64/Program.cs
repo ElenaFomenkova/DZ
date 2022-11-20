cfe506f93681b6c85544aace3c13939582bac60d
Elena <Fomenkova.len@ya.ru>
2022-11-21 00:58:55 +0300

    zadacha64

---
+++ DZ9/zadacha64/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+string RecurNumbers(int n)
+{
+    if (n == 1)
+    {
+        return "1";
+    }
+    else
+    {
+        return n + ", " + RecurNumbers (n - 1);
+    }
+}
+WriteLine(RecurNumbers(7));
