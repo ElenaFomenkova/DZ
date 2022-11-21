1ed9c4d50e54000276849a8e030c2f3b0c3f4d86
Elena <Fomenkova.len@ya.ru>
2022-11-21 17:23:18 +0300

    zadacha68

---
+++ DZ9/zadacha68/Program.cs
@@ -0,0 +1,18 @@
+﻿int Akk(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else
+      if ((n == 0) & (m > 0))
+    {
+        return Akk(m - 1, 1);
+    }
+    else
+    {
+        return Akk(m - 1, Akk(m, n - 1));
+    }
+}
+
+System.Console.WriteLine(Akk(3, 2));
